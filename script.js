585df9dc11e14497976f102ecc98200f7c7ddb84
Soumyareddy2004 <sowmyareddy1911@gmail.com>
2025-04-05 03:53:28 +0530

    Updated projects section

--- script.js
+++ script.js
@@ -68,10 +68,30 @@ var typed = new Typed(".multiple-text", {
   function irisLive() {
     open("");
   }
-  function tastyPlate() {
-    open("");
+  function productify() {
+    open("https://github.com/Soumyareddy2004/productify");
+  }
+  function noPovertyCode() {
+    window.open("https://github.com/Soumyareddy2004/No-Poverty", "_blank");
+  }
+  function eduOrbitCode() {
+    window.open("https://github.com/Soumyareddy2004/EduOrbit", "_blank");
   }
   
+  function eduOrbitLive() {
+    window.open("https://eduorbit-front.onrender.com/", "_blank"); 
+  }
+  
+  function noPovertyLive() {
+    window.open("https://no-poverty.onrender.com/tweet/", "_blank");
+  }
+  function geoVisionCode() {
+    window.open("https://github.com/YourUsername/geovision", "_blank");
+  }
+  
+  function geoVisionLive() {
+    window.open("https://your-live-link.com/geovision", "_blank");
+  }
  function resume() {
     // open("https://drive.google.com/file/d/1z8xWH8bqZRC793IJaqV7FufLixMWcUoj/view?usp=sharing");
     window.location.href='SoumyaReddy-resume.pdf';
